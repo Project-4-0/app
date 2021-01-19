8fd4dde3d92abdf45d1c4d3155d7b111453528c0
Arno <r0741327@student.thomasmore.be>
2021-01-19 14:15:43 +0100

    Verry basic home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:b_one_project_4_0/widgets/BottomAppBarBOne.dart';
 import 'package:b_one_project_4_0/widgets/FlatButtonBOne.dart';
+import 'package:b_one_project_4_0/widgets/DashboardButtonsOverview.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,72 +13,134 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      body: Container(
-        child: Stack(
-          children: [
-            ClipPath(
-              // clipper: CloudTopLeftClipper(),
-              child: Container(
-                color: Theme.of(context).primaryColorLight,
+      body: SingleChildScrollView(
+        child: Container(
+          child: Stack(
+            children: [
+              ClipPath(
+                // clipper: CloudTopLeftClipper(),
+                child: Container(
+                  color: Theme.of(context).primaryColorLight,
+                ),
               ),
-            ),
-            SafeArea(
-              child: Padding(
-                padding: EdgeInsets.all(20.0),
-                child: Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      RichText(
-                        text: TextSpan(
-                          text: '',
+              SafeArea(
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Dashboard',
                           style: TextStyle(
-                            color: Colors.black,
+                            color: Theme.of(context).primaryColor,
                             fontSize: 50,
                             fontWeight: FontWeight.bold,
                             fontFamily: 'Poppins',
                           ),
-                          children: [
-                            TextSpan(
-                              text: 'B',
-                              style: TextStyle(
-                                  color: Theme.of(context).primaryColor),
-                            ),
-                            TextSpan(
-                              text: ' One',
-                              style: TextStyle(
-                                color: Theme.of(context).accentColor,
+                        ),
+                        Padding(padding: EdgeInsets.all(15.0)),
+                        Container(
+                          width: double.infinity,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              // Users
+                              Expanded(
+                                child: RaisedButton(
+                                    onPressed: () {
+                                      Navigator.pushNamedAndRemoveUntil(
+                                          context, '/users', (route) => false);
+                                    },
+                                    color: Theme.of(context).buttonColor,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(5.0)),
+                                    padding: EdgeInsets.all(25.0),
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        Text('Gebruikers',
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 22.0)),
+                                        Icon(
+                                          Icons.group,
+                                          color: Colors.white,
+                                        ),
+                                      ],
+                                    )),
                               ),
-                            ),
-                          ],
+                              SizedBox(
+                                width: 10.0,
+                              ),
+                              // Boxen
+                              Expanded(
+                                child: RaisedButton(
+                                    onPressed: () {
+                                      Navigator.pushNamedAndRemoveUntil(
+                                          context, '/boxen', (route) => false);
+                                    },
+                                    color: Theme.of(context).buttonColor,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(5.0)),
+                                    padding: EdgeInsets.fromLTRB(
+                                        10.0, 25.0, 10.0, 25.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: <Widget>[
+                                        Text('Boxen',
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 22.0)),
+                                        Icon(
+                                          Icons.widgets,
+                                          color: Colors.white,
+                                        ),
+                                      ],
+                                    )),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      Container(
-                        child: Icon(
-                          Icons.camera_alt,
-                          color:
-                              Theme.of(context).indicatorColor.withOpacity(0.4),
-                          size: 300,
+                        Padding(padding: EdgeInsets.all(15.0)),
+                        SizedBox(
+                          width: 250.0,
+                          height: 150.0,
+                          child: const Card(child: Text('Graph!')),
                         ),
-                      ),
-                      Container(
-                        width: double.infinity,
-                        child: FlatButtonBOne(
-                          onPressed: () {
-                            Navigator.pushNamedAndRemoveUntil(
-                                context, '/scanProduct', (route) => false);
-                          },
-                          text: "Starten",
+                        Padding(padding: EdgeInsets.all(15.0)),
+                        SizedBox(
+                          width: 250.0,
+                          height: 150.0,
+                          child: const Card(child: Text('Graph!')),
                         ),
-                      ),
-                    ],
+                        Padding(padding: EdgeInsets.all(15.0)),
+                        SizedBox(
+                          width: 250.0,
+                          height: 150.0,
+                          child: const Card(child: Text('Sat!')),
+                        ),
+                        SizedBox(
+                          width: 250.0,
+                          height: 150.0,
+                          child: const Card(child: Text('Graph!')),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButtonBOne(),
