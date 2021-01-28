5b330a6363751940144f9e3a32c2e8e9c2280470
YouriVLTM <info@yourivanlaer.be>
2021-01-28 20:11:30 +0100

    fixed boxen same color

--- lib/widgets/charts/StackAreacLineChartBone.dart
+++ lib/widgets/charts/StackAreacLineChartBone.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 
 // Default design button BOne
-class StackAreacLineChartBone extends StatelessWidget {
+class StackAreacLineChartBone extends StatefulWidget {
   const StackAreacLineChartBone({
     Key key,
     @required this.measurementGraphics,
@@ -15,14 +15,20 @@ class StackAreacLineChartBone extends StatelessWidget {
   final String title;
 
   @override
+  _StackAreacLineChartBoneState createState() =>
+      _StackAreacLineChartBoneState();
+}
+
+class _StackAreacLineChartBoneState extends State<StackAreacLineChartBone> {
+  @override
   Widget build(BuildContext context) {
-    if (measurementGraphics == null) {
+    if (widget.measurementGraphics == null) {
       return Text("loading");
     }
     return Column(
       children: [
         Text(
-          this.title,
+          this.widget.title,
           style: TextStyle(fontSize: 25),
         ),
         SizedBox(
@@ -34,10 +40,60 @@ class StackAreacLineChartBone extends StatelessWidget {
                 includeArea: true, stacked: false),
             animate: false,
             dateTimeFactory: const charts.LocalDateTimeFactory(),
+            behaviors: [
+              new charts.SeriesLegend(
+                // Positions for "start" and "end" will be left and right respectively
+                // for widgets with a build context that has directionality ltr.
+                // For rtl, "start" and "end" will be right and left respectively.
+                // Since this example has directionality of ltr, the legend is
+                // positioned on the right side of the chart.
+                position: charts.BehaviorPosition.bottom,
+                // For a legend that is positioned on the left or right of the chart,
+                // setting the justification for [endDrawArea] is aligned to the
+                // bottom of the chart draw area.
+                outsideJustification: charts.OutsideJustification.endDrawArea,
+                // By default, if the position of the chart is on the left or right of
+                // the chart, [horizontalFirst] is set to false. This means that the
+                // legend entries will grow as new rows first instead of a new column.
+                horizontalFirst: false,
+                // By setting this value to 2, the legend entries will grow up to two
+                // rows before adding a new column.
+                desiredMaxRows: 2,
+                // This defines the padding around each legend entry.
+                cellPadding: new EdgeInsets.only(right: 4.0, bottom: 4.0),
+                // Render the legend entry text with custom styles.
+                entryTextStyle: charts.TextStyleSpec(fontSize: 11),
+              )
+            ],
           ),
         ),
       ],
     );
+  }
+
+  int colorCounter = 0;
+  _getRadomColor() {
+    // Random random = new Random();
+    // int colorCounter = random.nextInt(3);
+    colorCounter += 1;
+    switch (colorCounter) {
+      case 0:
+        return charts.MaterialPalette.gray.shadeDefault;
+      case 1:
+        return charts.MaterialPalette.blue.shadeDefault;
+      case 2:
+        return charts.MaterialPalette.red.shadeDefault;
+      case 3:
+        return charts.MaterialPalette.green.shadeDefault;
+      case 4:
+        return charts.MaterialPalette.gray.shadeDefault;
+      case 5:
+        return charts.MaterialPalette.green.shadeDefault;
+      case 6:
+        return charts.MaterialPalette.purple.shadeDefault;
+      default:
+        return charts.MaterialPalette.green.shadeDefault;
+    }
   }
 
   List<charts.Series<TimeSeriesSales, DateTime>> _transformBuildDate() {
@@ -45,21 +101,25 @@ class StackAreacLineChartBone extends StatelessWidget {
     // Alle boxen
     var boxendata = List<charts.Series<TimeSeriesSales, DateTime>>();
 
+    //reset color
+    colorCounter = 0;
+
     //foreach boxen get Graph
-    this.measurementGraphics.boxes.forEach((box) {
+    this.widget.measurementGraphics.boxes.forEach((box) {
       var col = _getRadomColor();
       myFakeDesktopData = List<TimeSeriesSales>();
       for (int i = 0;
-          i < this.measurementGraphics.measurementsList.length;
+          i < this.widget.measurementGraphics.measurementsList.length;
           i++) {
-        if (box.id == this.measurementGraphics.measurementsList[i].boxID) {
+        if (box.id ==
+            this.widget.measurementGraphics.measurementsList[i].boxID) {
           // print(this.measurementGraphics.measurementsList[i].boxID);
           // print(this.measurementGraphics.measurementsList[i].value);
           myFakeDesktopData.add(
             new TimeSeriesSales(
-                this.measurementGraphics.measurementsList[i].timeStamp,
+                this.widget.measurementGraphics.measurementsList[i].timeStamp,
                 double.parse(
-                    this.measurementGraphics.measurementsList[i].value)),
+                    this.widget.measurementGraphics.measurementsList[i].value)),
           );
         }
       }
@@ -78,22 +138,6 @@ class StackAreacLineChartBone extends StatelessWidget {
 
     //return all boxen graph
     return boxendata;
-  }
-
-  // get color
-  _getRadomColor() {
-    Random random = new Random();
-    int colorCounter = random.nextInt(3);
-    switch (colorCounter) {
-      case 1:
-        return charts.MaterialPalette.blue.shadeDefault;
-      case 2:
-        return charts.MaterialPalette.red.shadeDefault;
-      case 3:
-        return charts.MaterialPalette.green.shadeDefault;
-      default:
-        return charts.MaterialPalette.green.shadeDefault;
-    }
   }
 }
 
