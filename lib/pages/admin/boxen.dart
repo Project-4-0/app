a53fe502c08f1504f81a2b011f48c650eaacc5b8
Arno <r0741327@student.thomasmore.be>
2021-01-28 13:25:52 +0100

    Show sensors

--- lib/pages/admin/boxen.dart
+++ lib/pages/admin/boxen.dart
@@ -149,140 +149,180 @@ class _BoxenOverviewPage extends State {
   void _boxDetail(context, Box box) {
     // GlobalKey globalKey = new GlobalKey();
 
-    box!=null ? 
-    showModalBottomSheet(
-      isScrollControlled: true, // Full screen height
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(
-          top: Radius.circular(30),
-        ),
-      ),
-      clipBehavior: Clip.antiAliasWithSaveLayer,
-      backgroundColor: Colors.white,
-      context: context,
-      builder: (BuildContext context) {
-        return StatefulBuilder(
-            builder: (BuildContext context, StateSetter setState) {
-          return SingleChildScrollView(
-              child: Container(
-            // height: 900,
-            color: Colors.white,
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 5.0),
-              child: Column(
-                children: [
-                  FractionalTranslation(
-                      translation: Offset(0.0, 0.0),
-                      child: Align(
-                        child: Stack(children: <Widget>[
-                          CircleAvatar(
-                              radius: 50.0,
-                              backgroundColor: Theme.of(context).primaryColor,
-                              child: Padding(
-                                padding: EdgeInsets.all(16.0),
-                                child: Text(box.name,
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold),
-                                    textAlign: TextAlign.center),
-                              )),
-                          Positioned(
-                            // Marble to show active status
-                            top: 0.0,
-                            right: 0.0,
-                            child: Icon(Icons.brightness_1,
-                                size: 30.0,
-                                color: box.active ? Colors.green : Colors.red),
-                          )
-                        ]),
-                        alignment: FractionalOffset(0.5, 0.0),
-                      )),
-                  Padding(padding: EdgeInsets.all(20)),
-                  // Values of box
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Text(
-                        "Mac adress:\n" + box.macAddress,
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      Padding(padding: EdgeInsets.all(5)),
-                      Text(
-                        "Locatie:\n" +
-                            "Harmoniestraat 44 !!!\n" +
-                            "2230 Ramsel",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      Padding(padding: EdgeInsets.all(5)),
-                      Text(
-                        "In gebruik door:\n" + "Arno Vangoetsenhoven !!!",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      if (box.comment != null)
-                        Padding(padding: EdgeInsets.all(5)),
-                      if (box.comment != null)
-                        Text(
-                          "Opmerking:\n" + box.comment,
-                          style: TextStyle(fontSize: 18),
-                        ),
-                      Padding(padding: EdgeInsets.all(5)),
-                      OutlineFlatButtonBOne(
-                        text: "Wijzigen",
-                        onPressed: () {
-                          print("Go to edit page of box");
-                        },
-                      ),
-                      Padding(padding: EdgeInsets.all(5)),
-                      Column(
-                        children: <Widget>[
-                          RepaintBoundary(
-                              key: globalKey,
-                              child: QrImage(
-                                data: box.macAddress,
-                                version: QrVersions.auto,
-                                size: 150,
-                                gapless: false,
-                              )),
-                          IconButton(
-                            icon: Icon(Icons.share,
-                                color: Theme.of(context).primaryColor),
-                            tooltip: 'Increase volume by 10',
-                            onPressed: () {
-                              print("Share QR-Code");
-                              _captureAndSharePng(box.name);
-                            },
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ],
+    box != null
+        ? showModalBottomSheet(
+            isScrollControlled: true, // Full screen height
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(30),
               ),
             ),
-          ));
-        });
-      },
-    ) : 
-        showModalBottomSheet(
-      isScrollControlled: true, // Full screen height
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(
-          top: Radius.circular(30),
-        ),
-      ),
-      clipBehavior: Clip.antiAliasWithSaveLayer,
-      backgroundColor: Colors.white,
-      context: context,
-      builder: (BuildContext context) {
-        return StatefulBuilder(
-            builder: (BuildContext context, StateSetter setState) {
-          return SingleChildScrollView(
-              child: Container(
-            // height: 900,
-            color: Colors.white,
-            child: Center(child: CircularProgressIndicator()),
-          ));
-        });
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            backgroundColor: Colors.white,
+            context: context,
+            builder: (BuildContext context) {
+              return StatefulBuilder(
+                  builder: (BuildContext context, StateSetter setState) {
+                return SingleChildScrollView(
+                    child: Container(
+                  // height: 900,
+                  color: Colors.white,
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 5.0),
+                    child: Column(
+                      children: [
+                        FractionalTranslation(
+                            translation: Offset(0.0, 0.0),
+                            child: Align(
+                              child: Stack(children: <Widget>[
+                                CircleAvatar(
+                                    radius: 50.0,
+                                    backgroundColor:
+                                        Theme.of(context).primaryColor,
+                                    child: Padding(
+                                      padding: EdgeInsets.all(16.0),
+                                      child: Text(box.name,
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold),
+                                          textAlign: TextAlign.center),
+                                    )),
+                                Positioned(
+                                  // Marble to show active status
+                                  top: 0.0,
+                                  right: 0.0,
+                                  child: Icon(Icons.brightness_1,
+                                      size: 30.0,
+                                      color: box.active
+                                          ? Colors.green
+                                          : Colors.red),
+                                )
+                              ]),
+                              alignment: FractionalOffset(0.5, 0.0),
+                            )),
+                        Padding(padding: EdgeInsets.all(20)),
+                        // Values of box
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Text(
+                              "Mac adress:\n" + box.macAddress,
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            Padding(padding: EdgeInsets.all(5)),
+                            Text(
+                              "Locatie:\n" +
+                                  "Harmoniestraat 44 !!!\n" +
+                                  "2230 Ramsel",
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            Padding(padding: EdgeInsets.all(5)),
+                            Text(
+                              "In gebruik door:\n" + "Arno Vangoetsenhoven !!!",
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            if (box.comment != null)
+                              Padding(padding: EdgeInsets.all(5)),
+                            if (box.comment != null)
+                              Text(
+                                "Opmerking:\n" + box.comment,
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            Padding(padding: EdgeInsets.all(5)),
+                            Text(
+                              "Sensors:",
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            Container(
+                              child: _sensorList(context, box.sensors),
+                            ),
+                            Padding(padding: EdgeInsets.all(5)),
+                            OutlineFlatButtonBOne(
+                              text: "Wijzigen",
+                              onPressed: () {
+                                print("Go to edit page of box");
+                              },
+                            ),
+                            Padding(padding: EdgeInsets.all(5)),
+                            Column(
+                              children: <Widget>[
+                                RepaintBoundary(
+                                    key: globalKey,
+                                    child: QrImage(
+                                      data: box.macAddress,
+                                      version: QrVersions.auto,
+                                      size: 150,
+                                      gapless: false,
+                                    )),
+                                IconButton(
+                                  icon: Icon(Icons.share,
+                                      color: Theme.of(context).primaryColor),
+                                  tooltip: 'Increase volume by 10',
+                                  onPressed: () {
+                                    print("Share QR-Code");
+                                    _captureAndSharePng(box.name);
+                                  },
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ));
+              });
+            },
+          )
+        : showModalBottomSheet(
+            isScrollControlled: true, // Full screen height
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(30),
+              ),
+            ),
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            backgroundColor: Colors.white,
+            context: context,
+            builder: (BuildContext context) {
+              return StatefulBuilder(
+                  builder: (BuildContext context, StateSetter setState) {
+                return SingleChildScrollView(
+                    child: Container(
+                  // height: 900,
+                  color: Colors.white,
+                  child: Center(child: CircularProgressIndicator()),
+                ));
+              });
+            },
+          );
+  }
+
+  ListView _sensorList(context, sensorList) {
+    return new ListView.builder(
+      primary: false,
+      shrinkWrap: true,
+      physics: const AlwaysScrollableScrollPhysics(), // new
+      scrollDirection: Axis.vertical,
+      itemCount: sensorList.length,
+      itemBuilder: (context, int position) {
+        return Padding(padding: EdgeInsets.only(top: 5.0),
+          child: Row(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(15.0, 0, 5.0, 0),
+              child: Icon(
+                Icons.settings_input_hdmi,
+                color: Theme.of(context).primaryColor,
+                size: 14,
+              ),
+            ),
+            Container(
+                child: Flexible(
+                    child: Text(sensorList[position].name,
+                        textAlign: TextAlign.left))),
+          ],
+        ));
       },
     );
   }
